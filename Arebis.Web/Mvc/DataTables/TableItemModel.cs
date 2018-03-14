a8e8c785302740b37045d8b8f5f89e985b8bb618
codetuner <rudi.breedenraedt@arebis.be>
2018-03-14 23:37:47 +0100

    Refactored Mvc.DataTables

---
+++ Arebis.Web/Mvc/DataTables/TableItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arebis.Web.Mvc.DataTables
+{
+    public abstract class TableItemModel<TTableItem>
+    {
+        public TTableItem Item { get; set; }
+    }
+}
