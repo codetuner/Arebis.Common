8157160c5ba6d108e7b4402e25d6f8a37a82ea75
codetuner <codetuner@users.noreply.github.com>
2024-09-06 14:02:12 +0200

    Arebis.Common 4.8 released

--- Arebis.Contract/Properties/AssemblyInfo.cs
+++ Arebis.Contract/Properties/AssemblyInfo.cs
@@ -28,5 +28,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")] // Assembly version
+[assembly: AssemblyInformationalVersion("4.8.0.0")] // Nuget version, must be X.Y.0.0
 [assembly: System.CLSCompliant(true)]
--- Arebis.Logging.GrayLog/Properties/AssemblyInfo.cs
+++ Arebis.Logging.GrayLog/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.5.0.0")] // Assembly version
-[assembly: AssemblyInformationalVersion("1.9.0.0")] // Nuget version, must be X.Y.0.0
+[assembly: AssemblyInformationalVersion("4.8.0.0")] // Nuget version, must be X.Y.0.0
 [assembly: System.CLSCompliant(true)]
